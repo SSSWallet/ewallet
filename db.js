ef8ce7892b8322f648ab0b429035eeac9a662e2a
SSSWallet <ewallet@outlook.it>
2026-06-07 19:11:46 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,14 @@
+const { Pool } = require('pg');
+require('dotenv').config();
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Richiesto da Supabase per le connessioni sicure
+  }
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  getClient: () => pool.connect() // Ci servirà per le transazioni sicure
+};
