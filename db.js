55bd72f882bde12065625fedec9a3a089daaefe4
SSSWallet <ewallet@outlook.it>
2026-06-10 21:05:10 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres', // Torna l'utente semplice
-  host: 'db.rvsgbsnkurutsburxkwk.supabase.co', // Il tuo host originale
+  user: 'postgres',
+  host: '54.93.53.117', // <--- L'IP IPv4 diretto di Supabase (Francoforte / AWS)
   database: 'postgres',
   password: 'Bracciano.2026', 
-  port: 5432, // Torniamo alla porta standard
+  port: 5432, 
   ssl: {
     rejectUnauthorized: false 
   },
-  connectionTimeoutMillis: 10000 
+  connectionTimeoutMillis: 15000 // Aumentiamo a 15 secondi per sicurezza
 });
 
 module.exports = {
